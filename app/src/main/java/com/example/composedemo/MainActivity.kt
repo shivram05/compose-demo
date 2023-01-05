4d57ded61dc7fe28e41e21904821b32da4b93a6a
shivram <shivramshrestha01@gmail.com>
2023-01-04 21:44:35 -0800

    card view with image

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -3,38 +3,87 @@ package com.example.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-         Column(modifier = Modifier
-             .background(Color.Green)
-             .fillMaxHeight()
-             .fillMaxWidth()
-             .padding(10.dp)
-             .border(
-                 5.dp, Color.Red
-             )
-         ) {
-             Text(text = "hello", modifier = Modifier
-                 .padding(20.dp)
-                 .clickable {
-                     println("Clicking data")
-                 })
-             Spacer(modifier = Modifier.height(50.dp))
-             Text(text = "world")
-         }
+            val painter = painterResource(id = R.drawable.test_image)
+            val description = getString(R.string.test)
+            val title = "Testing data"
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
         }
     }
 }
 
-//
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.clip(RoundedCornerShape(15.dp))
+            .fillMaxWidth()
+            .clickable {
+                println("card clickable")
+            },
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+//        to make stack
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter, contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+
+
+                        )
+                ))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+                , contentAlignment = Alignment.BottomStart){
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+
+    }
+}
