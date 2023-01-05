b3b180cff82ef45cca449c8cca0fe3901ba12bf7
shivram <shivramshrestha01@gmail.com>
2023-01-04 22:09:03 -0800

    doing some styling

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,35 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val fontFamily = FontFamily(
+            Font(R.font.lexenddecablack, FontWeight.Black),
+            Font(R.font.lexenddecabold, FontWeight.Bold)
+        )
         setContent {
-         Column(modifier = Modifier
-             .background(Color.Green)
-             .fillMaxHeight()
-             .fillMaxWidth()
-             .padding(10.dp)
-             .border(
-                 5.dp, Color.Red
-             )
-         ) {
-             Text(text = "hello", modifier = Modifier
-                 .padding(20.dp)
-                 .clickable {
-                     println("Clicking data")
-                 })
-             Spacer(modifier = Modifier.height(50.dp))
-             Text(text = "world")
-         }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black)
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Gray,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                              append("J")
+                        }
+                        append("etpack")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily
+                )
+            }
         }
     }
 }
