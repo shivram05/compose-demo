8d129f4045813525030c0bb787ac0da9cb6fa5e1
shivram <shivramshrestha01@gmail.com>
2023-01-05 17:14:13 -0800

    state is maked by dividing two screen block

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -1,40 +1,56 @@
 package com.example.composedemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-         Column(modifier = Modifier
-             .background(Color.Green)
-             .fillMaxHeight()
-             .fillMaxWidth()
-             .padding(10.dp)
-             .border(
-                 5.dp, Color.Red
-             )
-         ) {
-             Text(text = "hello", modifier = Modifier
-                 .padding(20.dp)
-                 .clickable {
-                     println("Clicking data")
-                 })
-             Spacer(modifier = Modifier.height(50.dp))
-             Text(text = "world")
-         }
+            val color = remember {
+               mutableStateOf(Color.Yellow)
+            }
+            Column() {
+
+                colorBox(modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()){
+                    color.value = it
+                }
+
+                Box(modifier = Modifier.background(color.value)
+                    .weight(1f).fillMaxSize()
+                )
+            }
+
         }
     }
 }
 
-//
+@SuppressLint("UnrememberedMutableState", "ComposableNaming")
+@Composable
+fun colorBox(modifier: Modifier,
+             updateColor : (Color)->Unit){
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            ))
+        }
+    )
+}
