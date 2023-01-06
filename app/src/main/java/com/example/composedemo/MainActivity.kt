d5e340f1ce5f6c66bc97298d8d487ad62e686b2e
shivram <shivramshrestha01@gmail.com>
2023-01-05 17:52:28 -0800

    list scrollable

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -3,38 +3,31 @@ package com.example.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-         Column(modifier = Modifier
-             .background(Color.Green)
-             .fillMaxHeight()
-             .fillMaxWidth()
-             .padding(10.dp)
-             .border(
-                 5.dp, Color.Red
-             )
-         ) {
-             Text(text = "hello", modifier = Modifier
-                 .padding(20.dp)
-                 .clickable {
-                     println("Clicking data")
-                 })
-             Spacer(modifier = Modifier.height(50.dp))
-             Text(text = "world")
-         }
+            LazyColumn{
+                items(5000){
+                    Text(
+                        text = "Item $it",
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(vertical = 20.dp)
+                    )
+                }
+            }
         }
     }
 }
-
-//
